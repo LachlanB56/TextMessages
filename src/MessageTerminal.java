1b7724360730ffba24bcd47234e7de8496e13de8
Lachlan Biersteker <Lachlan.biersteker1@gmail.com>
2026-05-28 11:02:06 -0700

    Added thread to read live.

--- src/MessageTerminal.java
+++ src/MessageTerminal.java
@@ -9,21 +9,24 @@ public class MessageTerminal {
     public static void main(String[] args) throws IOException {
         Scanner myscan = new Scanner(System.in);
         boolean open = true;
+        Thread readerThread = new Thread(MessageTerminal::liveRead);
+        readerThread.start();
 
-        while(open) {
+
+
+        while (open) {
             String username;
             username = checkentry();
             boolean entry = true;
 
-
-            while(entry) {
+            while (entry) {
                 System.out.println("Press 1 to send a text, 2 to view all texts and 3 to exit");
                 int i = myscan.nextInt();
                 if (i == 1) {
                     sendtext(username);
                 } else if (i == 2) {
                     readtext();
-                } else if(i == 3){
+                } else if (i == 3) {
                     entry = false;
                     open = exit();
                 } else {
@@ -34,13 +37,12 @@ public class MessageTerminal {
     }
 
 
-
     public static String checkentry() throws IOException {
         Scanner myscan = new Scanner(System.in);
         System.out.println("Welcome to the Chat terminal please press 1 to enter your user/password or 2 to create an account");
         boolean entry = true;
         String user = "n";
-        while(entry) {
+        while (entry) {
             int i = myscan.nextInt();
             if (i == 1) {
                 user = login();
@@ -69,25 +71,24 @@ public class MessageTerminal {
         System.out.println("please confirm password");
         String confirmpassword = myscan.nextLine();
 
-            while (!password.equals(confirmpassword)){
-                System.out.println("passwords do not match please try again");
+        while (!password.equals(confirmpassword)) {
+            System.out.println("passwords do not match please try again");
 
-                System.out.println("Please create a password");
-                password = myscan.nextLine();
+            System.out.println("Please create a password");
+            password = myscan.nextLine();
 
-                System.out.println("please confirm password");
-                confirmpassword = myscan.nextLine();
+            System.out.println("please confirm password");
+            confirmpassword = myscan.nextLine();
         }
         try {
             FileWriter mywriter = new FileWriter("Accounts.txt", true);
             mywriter.write(username + "|" + password + System.lineSeparator());
             mywriter.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("File not found");
         }
         System.out.println("Thank you, your username and password have been recorded!");
     }
-
 
 
     public static String login() throws FileNotFoundException {
@@ -102,27 +103,25 @@ public class MessageTerminal {
         try {
             BufferedReader myread = new BufferedReader(new FileReader("Accounts.txt"));
 
-            while((Line = myread.readLine()) != null){
+            while ((Line = myread.readLine()) != null) {
                 accounts.add(Line);
             }
-}
-        catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Sorry File not found");
-}
+        }
 
-        while(!accounts.contains(username + "|" + password)){
-        System.out.println("Username/password combination does not exist try again");
-        System.out.println("Please enter your username");
-        username = myscan.nextLine();
+        while (!accounts.contains(username + "|" + password)) {
+            System.out.println("Username/password combination does not exist try again");
+            System.out.println("Please enter your username");
+            username = myscan.nextLine();
 
-        System.out.println("Please enter your password");
-        password = myscan.nextLine();
+            System.out.println("Please enter your password");
+            password = myscan.nextLine();
         }
 
         System.out.print("Welcome " + username + "!" + System.lineSeparator());
         return username;
     }
-
 
 
     public static void sendtext(String username) throws IOException {
@@ -138,30 +137,52 @@ public class MessageTerminal {
             FileWriter writer = new FileWriter("texts.txt", true);
             writer.write(texts.username + "|" + texts.texts + "[" + texts.times + "]" + System.lineSeparator());
             writer.close();
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error file not found");
         }
 
     }
-    public static void readtext()throws IOException{
+
+    public static void readtext() throws IOException {
         List<String> texts = new ArrayList<>();
         String line = "n";
-       try {
-           BufferedReader myread = new BufferedReader(new FileReader("texts.txt"));
-           while((line = myread.readLine()) != null){
-               texts.add(line);
-           }
-           for (int i = 0; i < texts.size(); i++){
-               System.out.println(texts.get(i));
-           }
-       }catch(IOException e){
-           System.out.println("text file could not be found");
-       }
-
+        try {
+            BufferedReader myread = new BufferedReader(new FileReader("texts.txt"));
+            while ((line = myread.readLine()) != null) {
+                texts.add(line);
+            }
+            for (int i = 0; i < texts.size(); i++) {
+                System.out.println(texts.get(i));
+            }
+        } catch (IOException e) {
+            System.out.println("text file could not be found");
+        }
 
 
     }
-    public static boolean exit(){
-         return false;
+
+    public static boolean exit() {
+        return false;
+    }
+    public static void liveRead() {
+        String line = "";
+        String currentlast = "";
+        String Lastmessage = "";
+        while (true) {
+            try {
+                Thread.sleep(2000);
+                BufferedReader myread = new BufferedReader(new FileReader("texts.txt"));
+                while ((line = myread.readLine()) != null) {
+                    currentlast = line;
+                }
+                if(!currentlast.equals(Lastmessage)){
+                    System.out.println(currentlast);
+                    Lastmessage = currentlast;
+                }
+                myread.close();
+            } catch (IOException | InterruptedException e) {
+                System.out.println("File not found");
+            }
+        }
     }
 }
